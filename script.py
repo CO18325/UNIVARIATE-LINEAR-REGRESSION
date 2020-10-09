0c7d1f5c02cd92556f01f1f2f81af5d8b5c1a8c2
Inderpreet Singh <inderpreet221099@gmail.com>
2020-10-09 12:53:40 +0530

    UPDATED THE COMMENTING

--- script.py
+++ script.py
@@ -21,9 +21,12 @@ from mpl_toolkits.mplot3d import Axes3D
 # THIS WILL HELP US TO UNDERSTAND THE DATA BETTER
 def visualize_raw_data(data):
     
+    # X-AXIS WILL CONTAIN THE POPULATION
+    # Y-AXIS WILL CONTAIN THE PROFIT
     x = 'Population'
     y = 'Profit'
     title = 'POPULATION(in 10000) V/S PROFIT(in $10000)'
+    # NOW WE CALL THE FUNCTION FROM SNS LIBRARY
     graph = sns.scatterplot(x=x,y=y,data=data)
     graph.set_title(title)
 
@@ -41,10 +44,17 @@ def visualize_raw_data(data):
 #COST J(theta) IS PARAMETRIZED BY THETA MATRICE AND NOT X or y!!!
 def cost_function(X,y,theta):
     
-    m = len(y) # I.E. NO. OF ENTRES IN THE DATA SET
+    # I.E. NO. OF ENTRES IN THE DATA SET
+    m = len(y) 
+
+    # THIS IS THE MATHEMATICAL DOT PRODUCT 
+    # TO OBTAIN PREDICTION WRT TO THETA AND X
     y_pred = X.dot(theta)
+
+    # FIND THE SQUARED ERROR
     sq_error = (y_pred - y) ** 2
 
+    # AND THEN WE FINALLY RETURN THE COST FUNCTION CALCULATED 
     return 1/(m * 2) * np.sum(sq_error)
 
 
@@ -58,12 +68,31 @@ def cost_function(X,y,theta):
 def gradient_descent(X,y,theta,alpha,iterations):
     
     m = len(y)
+
+    # WE WILL STORE THE COST FUNCTIONS CALCULATED EACH TIME HERE
+    # THIS IS JUST TO VISUALIZE OUR CONVERGENCE TOWARDS OPTIMAL COST FUNCTION
+    # WRT THE ITERATIONS DONE 
     costs = []
+
     for i in range(0,iterations):
+        
+        # CALCULATE THE PREDICTION WRT TO theta
         y_pred = X.dot(theta)
+
+        # CALCULATE THE SUBMASSION PART OF THE GRADIENT DESENCT
+        # GO BACK TO SLIDE
         error = np.dot(X.transpose(),(y_pred - y))
+
+        # CHANGE THE THETA FOR ACHEIVING THE OPTIMAL COST FUNCTION
         theta -= alpha * error * (1/m)
+
+        # STORE THE COST FUNCTION OBTAINED IN OUR costs VARIABLE
+        # FOR FUTURE VISULAIZATIONS
         costs.append(cost_function(X,y,theta))
+
+
+    # AND FINALLY RETURN THE OPTIMAL THETA FOUND
+    # AND THE COSTS STORED OVER ITERATIONS        
     return theta,costs
 
 
@@ -78,30 +107,43 @@ def gradient_descent(X,y,theta,alpha,iterations):
 # THE PURPOSE OF VISUALIZATION IS TO SEE HOW GRADIENT FUNCTION WILL MOVE
 def visualize_cost_function(X, y):
     
+    # THE BELOW FUNCTION np.linespace DEFNES THAT
+    # RETURN 100 EQUISACED VALUES B/W -10 AND 10
     theta_0 = np.linspace(-10,10,100)
     theta_1 = np.linspace(-1,4,100)
 
-    cost_values = np.zeros((len(theta_0),len(theta_1))) #MATRICE OF SIZE THETA_0 X THETA_1
+    # DEFINE A MATRICE TO STORE THETA VALUES
+    #MATRICE OF SIZE THETA_0 X THETA_1
+    # THIS IS DONE TO INCORPORATE EACH AND EVERY COMBINATION OF THETAS
+    cost_values = np.zeros((len(theta_0),len(theta_1)))
 
     for i in range(len(theta_0)):
         for j in range(len(theta_1)):
+            # GET CURRENT THETA VALUES
             specific_theta = np.array([theta_0[i], theta_1[j]])
+             # CALCULATE THE COST BASED ON THE ABOVE DUMMY THETA VALUES
             cost_values[i,j] = cost_function(X, y, specific_theta)
     
+    # NOW WE CAN FORM THE GRAPH
     graph_formation(theta_0,theta_1,cost_values)
 
 
 def graph_formation(theta_0,theta_1,cost_values):
     
+
     fig = plt.figure(figsize=(12,8))
+    # DEFINIG GRAPH TYPE
     graph = fig.gca(projection='3d')
 
     surf = graph.plot_surface(theta_0,theta_1,cost_values,cmap='viridis')
+    # COLORBAR OR THE LEGEND
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.xlabel('THETA_0')
     plt.ylabel('THETA_1')
     graph.set_zlabel('J OF THETA')
-    graph.view_init(30,330) # TO GIVE INITIAL ANGLE TO THE GRAPH FOR BETTER VIEW
+
+    # TO GIVE INITIAL ANGLE TO THE GRAPH FOR BETTER VIEW
+    graph.view_init(30,330) 
 
     plt.show()
 
@@ -182,22 +224,33 @@ def main():
     # THIS IS THE SIZE OF THE WINDOW THAT WILL OPEN TO SHOW ALL THE GRAPHS
     plt.rcParams['figure.figsize'] = (12,8)
 
+
     # DATA RECEIVED
+    # DATA RECEIVED IS M0STLY IN THE FORM OF DATA FRAME
+    # DATA FRAME IS VERY USEFUL DATASTRUCTURE IN PANDAS LIBRARY IN TERMS OF DATA SET MANAGEMENT
     data = pd.read_csv('bike_sharing_data.txt')
-    # TO PRINT FIRST FIVE ENTRIES OF THE 
+
+    # TO PRINT FIRST FIVE ENTRIES OF THE DATASET TO UNDERSTAND THE DATA BETTER
     print(data.head())
 
     #POPULATION OF CITIES IN 10000s
     #PROFIT IN UNITS 10000 DOLLARS
     #TO GET MORE INFO ABOUT THE CSV FILE:-
+    # THIS WILL HELP US UNDERSTAND THE DATA TYPES OF THE DATA
+    # AND ALSO CHECK IF THERE IS ANY NULL VALUES PRESENT HERE
     print(data.info())
 
     #VISUALIZATION OF DATA 
     visualize_raw_data(data)
 
+
+    #######################################################################################
+
+
     #SETTING UP REGRESSION VARIABLES:
 
-    m = data.Population.values.size #I.E. NO. OF ENTRIES
+    #I.E. NO. OF ENTRIES
+    m = data.Population.values.size 
 
     #X is a Matrice of 1's and the Population Column
     # 1's is to accomodate the Theta0 i.e. the Interept
@@ -209,7 +262,15 @@ def main():
     
     #theta is a 2*1 Matrice
     #Initializing the values of theta with ZERO!
+    # Although it is not a very good practice 
     theta = np.zeros((2,1))
+
+    # JUST TO TEST THE COST FUNCTION
+    print(cost_function(X, y, theta))
+
+   
+    #######################################################################################
+
 
     #LEARNING RATE
     alpha = 0.01
@@ -221,6 +282,7 @@ def main():
 
     # TO CHECK THE VALUE OF h(x) AND FINAL VALUE OF THE THETA MATRICE
     print("h(x) = {} + {}x1".format(str(round(theta[0,0],2)), str(round(theta[1,0],2))))
+
 
     #VISUALIZE THE COST FUNCTION WITH EXPLICIT THETA VALUES IN A 3D GRAPH
     visualize_cost_function(X, y)
@@ -243,7 +305,7 @@ def main():
     predicted_profit = predict_data(theta, input_population_matrice)
     print(predicted_profit)
 
-    print('PROFIT FOR POPULATION OF {} WOULD BE ${}'.format(str(round(input_population*10000)), str(round(predicted_profit*1000))))
+    print('PROFIT FOR POPULATION OF {} WOULD BE Rs{}'.format(str(round(input_population*10000)), str(round(predicted_profit*1000))))
 
 
 
